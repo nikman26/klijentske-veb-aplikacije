ef603888be17badd6c363f0f8e9544b01be33c03
nikman261 <nikolamanasijevic26@gmail.com>
2026-03-09 18:33:11 +0100

    Updated details component layout

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,16 +1,34 @@
 import { Component, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import axios from 'axios';
+import { FlightModel } from '../../models/flight.model';
+import { Utils } from '../utils';
+import { MatCardModule } from '@angular/material/card';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [MatCardModule, MatListModule, MatIconModule],
   templateUrl: './details.html',
   styleUrl: './details.css',
 })
 export class Details {
-  id = signal(null)
+  flight = signal<FlightModel | null>(null)
 
-  constructor(route: ActivatedRoute){
-    route.params.subscribe(params=>this.id.set(params['id']))
+  constructor(route: ActivatedRoute, public utils: Utils, private sanitizer: DomSanitizer) {
+    route.params.subscribe((params) => {
+      const id = params['id']
+      axios
+        .get(`https://flight.pequla.com/api/flight/${id}`)
+        .then((rsp) => this.flight.set(rsp.data))
+    });
+  }
+
+  getMapUrl(destination: string): SafeResourceUrl {
+    const encoded = encodeURIComponent(destination);
+    const url = `https://www.google.com/maps?q=${encoded}&output=embed`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
