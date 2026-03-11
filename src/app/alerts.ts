1838b6edee5004b1b431639f5f6773cc6b8e1220
nikman261 <nikolamanasijevic26@gmail.com>
2026-03-11 15:27:05 +0100

    Add recommended flights

--- src/app/alerts.ts
+++ src/app/alerts.ts
@@ -1,20 +1,48 @@
-import Swal from "sweetalert2";
+
+import Swal from 'sweetalert2';
+
+const matCustomClass = {
+    popup: 'mat-swal-popup',
+    title: 'mat-swa-title',
+    actions: 'mat-swal-actions',
+    confirmButton: 'mat-swal-confirm',
+    cancelButton: 'mat-swal-cancel'
+
+}
 
 export class Alerts {
-    static success(text: string){
-        Swal.fire({
-            title: 'Success',
-            text,
-            icon: 'success'
-        })
-    }
+  static success(text: string) {
+    Swal.fire({
+      title: 'Success',
+      text,
+      icon: 'success',
+      customClass: matCustomClass
+    });
+  }
 
-    static error(text: string){
-        Swal.fire({
-            title: 'Error',
-            text,
-            icon: 'error'
-        })
-    }
-    
+  static error(text: string) {
+    Swal.fire({
+      title: 'Error',
+      text,
+      icon: 'error',
+      customClass: matCustomClass
+    });
+  }
+
+  static confirm(text: string, callback: Function) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text,
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes',
+      customClass: matCustomClass
+    }).then((result) => {
+      if (result.isConfirmed) {
+        callback();
+      }
+    })
+  }
 }
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,68 +1,92 @@
 import { Component, signal } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { FlightService } from '../services/flight.service';
 import { Loading } from '../loading/loading';
 import { Alerts } from '../alerts';
+import { FlightModel } from '../../models/flight.model';
+import { MatListModule } from '@angular/material/list';
+import { Utils } from '../utils';
 
 @Component({
   selector: 'app-user',
-  imports: [MatCardModule, MatInputModule, MatButtonModule, MatIconModule,FormsModule, MatSelectModule, Loading],
+  imports: [
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatListModule,
+    MatSelectModule,
+    Loading,
+    RouterLink
+],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User {
-  public activeUser = AuthService.getActiveUser()
+  public activeUser = AuthService.getActiveUser();
   destinations = signal<string[]>([])
+  recommended = signal<FlightModel[]>([])
   oldPassword = ''
   newPassword = ''
   passRepeat = ''
 
-  constructor(private router: Router){
-    if (!AuthService.getActiveUser()){
-      router.navigate(['/login'])
-      return
+  constructor(private router: Router, public utils: Utils) {
+    if (!AuthService.getActiveUser()) {
+      router.navigate(['/login']);
+      return;
     }
 
-    FlightService.getDestinations()
-    .then(rsp=>this.destinations.set(rsp.data))
+    FlightService.getDestinations().then(rsp => this.destinations.set(rsp.data));
+
+    FlightService.getFlightToDestination(this.activeUser!.destination).
+    then(rsp=>this.recommended.set(rsp.data.content))
   }
 
-  updateUser(){
-    AuthService.updateActiveUser(this.activeUser!)
-    Alerts.success('User updated successfully')
+  getAvatarUrl(){
+    return `https://ui-avatars.com/api/?name=${this.activeUser?.firstName}+${this.activeUser?.lastName}`
   }
 
-  updatePassword(){
-    if (this.oldPassword != this.activeUser?.password){
-      Alerts.error('Invalid old password')
-      return
-    }
+  updateUser() {
+    Alerts.confirm('Are you sure you want to update user info?', () => {
+      AuthService.updateActiveUser(this.activeUser!);
+      Alerts.success('User updated successfully');
+    });
+  }
 
-    if (this.newPassword.length < 6){
-      Alerts.error('Password must be at least 6 characters long')
-      return
-    }
+  updatePassword() {
+    Alerts.confirm('Are you sure you want to change password?', () => {
+      if (this.oldPassword != this.activeUser?.password) {
+        Alerts.error('invlaid old password');
+        return;
+      }
 
-    if (this.newPassword != this.passRepeat){
-      Alerts.error('Password dont match')
-      return
-    }
+      if (this.newPassword.length < 6) {
+        Alerts.error('Password must be at least 6 characters long');
+        return;
+      }
 
-    if (this.newPassword == this.activeUser?.password){
-      Alerts.error('New password cant be the same as the old one')
-      return
-    }
+      if (this.newPassword != this.passRepeat) {
+        Alerts.error('Password dont match');
+        return;
+      }
 
-    AuthService.updateActiveUserPassword(this.newPassword)
-    Alerts.success('Password updated successfuly')
-    AuthService.logout()
-    this.router.navigate(['/login'])
+      if (this.newPassword == this.activeUser?.password) {
+        Alerts.error('New password cant be the same as the old one');
+        return;
+      }
+
+      AuthService.updateActiveUserPassword(this.newPassword);
+      Alerts.success('Password updated successfuly');
+      AuthService.logout();
+      this.router.navigate(['/login']);
+    });
   }
 }
