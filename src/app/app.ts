71bb022b92a572c3d4c7f265d334d46c4cf5261f
nikman261 <nikolamanasijevic26@gmail.com>
2026-08-28 14:15:15 +0200

    Add login function

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref, Router } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -18,4 +19,15 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  activeUser = AuthService.getActiveUser()
+
+constructor(private router: Router){
+
+}
+
+  doLogout(){
+    AuthService.logout()
+    this.router.navigate(['/login'])
+  }
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,16 +4,17 @@ const USERS = 'users'
 const ACTIVE = 'active'
 
 export class AuthService {
+
     static getUsers(): UserModel[] {
         if (localStorage.getItem(USERS) == null) {
-            localStorage.setItem(USERS, JSON.stringify({
+            localStorage.setItem(USERS, JSON.stringify([{
                 email: 'user@example.com',
                 password: 'user123',
                 destination: 'Zagreb',
                 firstName: 'Example',
                 lastName: 'User',
                 orders: []
-            }))
+            }]))
         }
 
         return JSON.parse(localStorage.getItem(USERS)!)
@@ -21,15 +22,20 @@ export class AuthService {
 
     static login(email: string, password: string) {
         const users = this.getUsers()
+
         for (let u of users) {
             if (u.email === email && u.password === password) {
                 localStorage.setItem(ACTIVE, email)
+                return true
             }
         }
+
+        return false
     }
 
     static getActiveUser(): UserModel | null {
         const users = this.getUsers()
+
         for (let u of users) {
             if (u.email === localStorage.getItem(ACTIVE)) {
                 return u
@@ -37,5 +43,9 @@ export class AuthService {
         }
 
         return null
+    }
+
+    static logout(){
+        localStorage.removeItem(ACTIVE)
     }
 }
